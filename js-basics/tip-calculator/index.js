e1ab9f4efecec889d4cd02ecc0f784ecf3908448
furmandarya1@gmail.com <furmandarya1@gmail.com>
2023-12-07 14:52:41 +0100

    finished challenge

--- js-basics/tip-calculator/index.js
+++ js-basics/tip-calculator/index.js
@@ -1,4 +1,7 @@
-const mealCost = 15;
+const mealCost = 20;
 const tipPersentage = 20;
 
 const tipAmount = mealCost * (tipPersentage / 100);
+const totalCost = mealCost + tipAmount;
+
+console.log(`The total cost of your bill is: ${totalCost} euros`);
