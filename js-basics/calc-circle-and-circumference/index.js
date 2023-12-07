b4567e30e8ea35f6cf86c62eea0b22611ee2bb9e
furmandarya1@gmail.com <furmandarya1@gmail.com>
2023-12-07 16:08:19 +0100

    circle area is...

---
+++ js-basics/calc-circle-and-circumference/index.js
@@ -0,0 +1,6 @@
+console.log("Calculate Circle Area and Circumference 🌶️🌶️🌶️");
+
+let circleRadius = 12;
+let circleArea = Math.PI * Math.pow(circleRadius, 2);
+
+console.log("Circle Area is ", circleArea);
