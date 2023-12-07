d6d0fa03b88e18f7ecb823c31731d282757a11fb
furmandarya1@gmail.com <furmandarya1@gmail.com>
2023-12-07 16:15:08 +0100

    circumference of circle is...

--- js-basics/calc-circle-and-circumference/index.js
+++ js-basics/calc-circle-and-circumference/index.js
@@ -4,3 +4,7 @@ let circleRadius = 12;
 let circleArea = Math.PI * Math.pow(circleRadius, 2);
 
 console.log("Circle Area is ", circleArea);
+
+let circleCircumference = 2 * Math.PI * circleRadius;
+
+console.log("Circumference of circle is ", circleCircumference);
