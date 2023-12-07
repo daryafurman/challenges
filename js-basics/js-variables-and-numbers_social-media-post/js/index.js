fd1e34a4b62ebe65ad040659661e154e98671c01
furmandarya1@gmail.com <furmandarya1@gmail.com>
2023-12-07 14:27:25 +0100

    fixing likes counter

--- js-basics/js-variables-and-numbers_social-media-post/js/index.js
+++ js-basics/js-variables-and-numbers_social-media-post/js/index.js
@@ -11,6 +11,12 @@ console.clear();
 
 // --v-- write your code here --v--
 
+const myTitle = "Hi, there";
+const myText = "Welcome ...";
+const theNumberOfLikes = 35; // initial number of likes
+const postCreator = "Daria Furman";
+const isReported = true;
+
 // --^-- write your code here --^--
 
 /*
@@ -18,5 +24,13 @@ console.clear();
 */
 
 // --v-- write your code here --v--
+
+console.log("myTitle: ", myTitle);
+console.log("myText: ", myText);
+console.log("Initial likes:", theNumberOfLikes);
+const updatedLikes = theNumberOfLikes + 1; // increase likes by one
+console.log("Updated likes:", updatedLikes);
+console.log("postCreator: ", postCreator);
+console.log("isReported: ", isReported);
 
 // --^-- write your code here --^--
