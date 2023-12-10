4552c15d14f9e1fe76c8c31b24c5f7940b5f94d9
furmandarya1@gmail.com <furmandarya1@gmail.com>
2023-12-10 20:49:03 +0100

    creating variables

--- js-basics/js-basics_dark-mode/js/index.js
+++ js-basics/js-basics_dark-mode/js/index.js
@@ -1,3 +1,7 @@
 console.clear();
 
 const bodyElement = document.querySelector('[data-js="body"]');
+
+const darkModeButton = document.querySelector('[data-js="dark-mode-button"]');
+const lightModeButton = document.querySelector('[data-js="light-mode-button"]');
+const toggleButton = document.querySelector('[data-js="toggle-button"]');
