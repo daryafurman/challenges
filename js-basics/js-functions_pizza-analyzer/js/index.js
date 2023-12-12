12c08651d166e40372404a87d87e22c984ca4a3e
furmandarya1@gmail.com <furmandarya1@gmail.com>
2023-12-12 13:55:06 +0100

    task 1.4 -2

--- js-basics/js-functions_pizza-analyzer/js/index.js
+++ js-basics/js-functions_pizza-analyzer/js/index.js
@@ -23,10 +23,44 @@ function calculatePizzaGain(diameter1, diameter2) {
   const area2 = Math.PI * Math.pow(diameter2 / 2, 2);
 
   const pizzaGain = ((area2 - area1) / area1) * 100;
+
+  const roundedPizzaGain = Math.round(pizzaGain);
+
+  output.textContent = roundedPizzaGain;
 }
+
+pizzaInput1.addEventListener("input", () => {
+  const pizzaSize1 = parseFloat(pizzaInput1.value);
+  const pizzaSize2 = parseFloat(pizzaInput2.value);
+  calculatePizzaGain(pizzaSize1, pizzaSize2);
+});
+
+pizzaInput2.addEventListener("input", () => {
+  // Save the current values of both pizza inputs
+  const pizzaSize1 = parseFloat(pizzaInput1.value);
+  const pizzaSize2 = parseFloat(pizzaInput2.value);
+
+  // Call the calculatePizzaGain function with the pizza sizes as arguments
+  calculatePizzaGain(pizzaSize1, pizzaSize2);
+});
 
 // Task 2
 // define the function updatePizzaDisplay here
+
+function updatePizzaDisplay(pizzaElement, newSize) {
+  const newDisplaySize = (newSize / 24) * 100;
+  pizzaElement.style.width = `${newDisplaySize}px`;
+}
+
+pizzaInput1.addEventListener("input", () => {
+  const pizzaSize1 = pizzaInput1.value;
+  updatePizzaDisplay(pizza1, pizzaSize1);
+});
+
+pizzaInput2.addEventListener("input", () => {
+  const pizzaSize2 = pizzaInput2.value;
+  updatePizzaDisplay(pizza2, pizzaSize2);
+});
 
 // Task 3
 // define the function updateOutputColor here
