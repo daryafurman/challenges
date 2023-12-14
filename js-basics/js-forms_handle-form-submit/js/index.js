190a67b40d3af80d622864291463ae864472b73b
furmandarya1@gmail.com <furmandarya1@gmail.com>
2023-12-14 11:27:43 +0100

    bonus

--- js-basics/js-forms_handle-form-submit/js/index.js
+++ js-basics/js-forms_handle-form-submit/js/index.js
@@ -1,12 +1,24 @@
 console.clear();
 
 const form = document.querySelector('[data-js="form"]');
+const age = document.querySelector('[data-js="age"]');
+const badness = document.querySelector('[data-js="badness"]');
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
   const formData = new FormData(event.target);
   const data = Object.fromEntries(formData);
-
+  //BONUS
+  const calculateAgePlusBadness = parseInt(data.age) + parseInt(data.badness);
+  console.log(`The age-badness-sum is ${calculateAgePlusBadness}`);
   console.log(data);
 });
+
+// ## Bonus: Calculate and log the age-badness-sum 🤷‍♀️
+
+// The formula is: `age + badness`
+
+// Log to the console _(replace the values between the quotes)_:
+
+// > The age-badness-sum of "firstName" is "age-badness-sum"
