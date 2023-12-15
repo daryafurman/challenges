b08eb3ff7109b05213fe632e3b6e4be19dd2c0a9
furmandarya1@gmail.com <furmandarya1@gmail.com>
2023-12-15 11:20:59 +0100

    challenge is complete

--- js-basics/js-createelement_social-media-post/js/index.js
+++ js-basics/js-createelement_social-media-post/js/index.js
@@ -10,3 +10,19 @@ likeButton.addEventListener("click", handleLikeButtonClick);
 
 // Exercise:
 // Use document.createElement() and append another social media post to the body.
+const newPost = document.createElement("post");
+newPost.innerHTML = `
+<section class="post">
+      <p class="post__content">
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+      </p>
+      <footer class="post__footer">
+        <span class="post__username">@username</span>
+        <button type="button" class="post__button" data-js="like-button">
+          ♥ Like
+        </button>
+      </footer>
+    </section>
+`;
+
+document.body.append(newPost);
