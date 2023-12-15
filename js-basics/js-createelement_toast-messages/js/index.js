498346ab5c5e2cafa421115136e9f955c98455c9
furmandarya1@gmail.com <furmandarya1@gmail.com>
2023-12-15 10:36:34 +0100

    clearButton works

--- js-basics/js-createelement_toast-messages/js/index.js
+++ js-basics/js-createelement_toast-messages/js/index.js
@@ -4,10 +4,25 @@ const toastContainer = document.querySelector('[data-js="toast-container"]');
 const addButton = document.querySelector('[data-js="add-button"]');
 const clearButton = document.querySelector('[data-js="clear-button"]');
 
-addButton.addEventListener("click", () => {
+addButton.addEventListener("click", (e) => {
   // Exercise: Append a new entry to the toast messages container
+  const newMessage = document.createElement("toast-container");
+
+  document.body.append(toastContainer);
 });
 
 clearButton.addEventListener("click", () => {
   // Exercise: Clear the stack of toast messages
+  toastContainer.remove();
 });
+
+// In the bottom right corner you can find a toast messages container with one entry.
+
+// When clicking the "Add toast message" button a new entry should be appended
+//to the toast messages container. Create the HTML element for a new toast message
+//with `document.createElement()`. Have a look at the `index.html` to see
+//the required HTML tag and class name.
+
+// When clicking the "Clear all toast messages" button, the list of toast messages
+//should be emptied. Afterwards you should still be able to add new toast messages
+//and clear them again.
