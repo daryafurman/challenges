7a9784037e51788d21002f0b9cc613c7756f2faa
furmandarya1@gmail.com <furmandarya1@gmail.com>
2023-12-17 13:12:43 +0100

    part 1

--- js-basics/js-loops_stars/js/index.js
+++ js-basics/js-loops_stars/js/index.js
@@ -7,6 +7,12 @@ function renderStars() {
   starContainer.innerHTML = "";
 
   //--v-- your code here --v--
+  for (let star = 1; star <= 5; star++) {
+    console.log(`iteration ${star}`);
+    const starElement = document.createElement("img");
+    starElement.src = "assets/star-empty.svg";
+    starContainer.append(starElement);
+  }
 
   //--^-- your code here --^--
 }
