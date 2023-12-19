8ff47eac7356277db510b25ac496d5bd75eaf9c7
furmandarya1@gmail.com <furmandarya1@gmail.com>
2023-12-19 15:42:13 +0100

    regular func

--- js-basics/js-callback-functions_user-login-callback-1-named-vs-anonymous/js/index.js
+++ js-basics/js-callback-functions_user-login-callback-1-named-vs-anonymous/js/index.js
@@ -1,8 +1,17 @@
 console.clear();
 
+function showWelcomeMessage() {
+  console.log("Welcome!");
+}
 function handleUserLogin(onSuccess) {
   // You might wonder why this function is called `onSuccess`. This will become clear by the fourth challenge! 🤞
   onSuccess();
 }
 
+function showWelcomeMessage() {
+  console.log("Welcome!");
+}
+
 // Call handleUserLogin below!
+// regular function
+handleUserLogin(showWelcomeMessage);
