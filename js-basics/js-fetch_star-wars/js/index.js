84d867ee5114d1cf9528e98c0ced6ff41f406a1c
furmandarya1@gmail.com <furmandarya1@gmail.com>
2024-01-08 17:44:24 +0100

    challenge complete

--- js-basics/js-fetch_star-wars/js/index.js
+++ js-basics/js-fetch_star-wars/js/index.js
@@ -41,6 +41,22 @@ fetchDataAndRender();
 
 // --v-- your code below this line --v--
 
-function fetchDataAndRender() {
-  fetch(); // ?
+async function fetchDataAndRender() {
+  const response = await fetch("https://swapi.dev/api/people");
+  const data = await response.json();
+  console.log(data);
 }
+
+fetchDataAndRender();
+
+async function fetchDataAndRenderTwo() {
+  const response = await fetch("https://swapi.dev/api/people");
+  const data = await response.json();
+
+  data.results.forEach((person) => {
+    const card = Card(person);
+    renderElement(card);
+  });
+}
+
+fetchDataAndRenderTwo();
