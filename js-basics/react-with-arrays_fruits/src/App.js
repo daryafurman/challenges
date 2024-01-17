00f3e6589f02b1a26a558af9b8da0e26f82a193f
furmandarya1@gmail.com <furmandarya1@gmail.com>
2024-01-17 15:35:35 +0100

    challenge complete

--- js-basics/react-with-arrays_fruits/src/App.js
+++ js-basics/react-with-arrays_fruits/src/App.js
@@ -1,11 +1,39 @@
 import Card from "./components/Card";
 
 export default function App() {
-  const fruits = [];
+  const fruits = [
+    {
+      id: 1337,
+      name: "🍌 Banana",
+      color: "yellow",
+    },
+    {
+      id: 1327,
+      name: "🍊 Orange",
+      color: "orange",
+    },
+    {
+      id: 1317,
+      name: "🍎 Apple",
+      color: "red",
+    },
+    {
+      id: 1357,
+      name: "🥝 Kiwi",
+      color: "green",
+    },
+    {
+      id: 1347,
+      name: "🍇 Grape",
+      color: "purple",
+    },
+  ];
 
   return (
     <div className="app">
-      <Card name="🍌 banana" />
+      {fruits.map((fruit) => {
+        return <Card key={fruit.id} {...fruit} />;
+      })}
     </div>
   );
 }
