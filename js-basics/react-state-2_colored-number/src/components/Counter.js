2fd884e2bb6f89f32d5e958866cd74931bf1833c
furmandarya1@gmail.com <furmandarya1@gmail.com>
2024-01-18 11:19:53 +0100

    challenge complete

---
+++ js-basics/react-state-2_colored-number/src/components/Counter.js
@@ -0,0 +1,35 @@
+import ColoredNumber from "./ColoredNumber";
+import { useState } from "react";
+
+export default function Counter() {
+  const [count, setCount] = useState(0);
+  console.log(count);
+  return (
+    <div className="counter">
+      <h1>Colored Number</h1>
+      <ColoredNumber value={count} />
+      <div className="counter__buttons-container">
+        <button
+          onClick={() => {
+            setCount(count + 1);
+          }}
+          type="button"
+          className="counter__button"
+          aria-label="increment count"
+        >
+          +
+        </button>
+        <button
+          onClick={() => {
+            setCount(count - 1);
+          }}
+          type="button"
+          className="counter__button"
+          aria-label="decrement count"
+        >
+          -
+        </button>
+      </div>
+    </div>
+  );
+}
