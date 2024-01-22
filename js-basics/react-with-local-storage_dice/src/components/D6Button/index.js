3315efe546edfcafca75c8dd28737a329cfd6420
furmandarya1@gmail.com <furmandarya1@gmail.com>
2024-01-22 11:33:44 +0100

    lifting state up

--- js-basics/react-with-local-storage_dice/src/components/D6Button/index.js
+++ js-basics/react-with-local-storage_dice/src/components/D6Button/index.js
@@ -3,18 +3,18 @@ import { getD6Roll } from "../../utils";
 import D6 from "../D6";
 import "./D6Button.css";
 
-export function D6Button() {
-  const [rolls, setRolls] = useState([]);
-
+export function D6Button({ value, onRoll }) {
+  // const [rolls, setRolls] = useState([]);
   const handleRoll = () => {
-    setRolls([{ value: getD6Roll(), time: Date.now() }, ...rolls]);
+    // setRolls([{ value: getD6Roll(), time: Date.now() }, ...rolls]);
+    onRoll();
   };
 
-  const currentRollValue = rolls[0]?.value;
+  // const currentRollValue = rolls[0]?.value;
 
   return (
     <button className="d6-button" type="button" onClick={handleRoll}>
-      <D6 value={currentRollValue} />
+      <D6 value={value} />
     </button>
   );
 }
